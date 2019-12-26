182af5d096205074823009925e6a47a0cd032bb2
Terry Sigle <terrysigle@pingidentity.com>
2019-12-26 10:52:24 -0700

    multi-cluster

--- baseline/pingdirectory/hooks/03-build-run-plan.sh
+++ baseline/pingdirectory/hooks/03-build-run-plan.sh
@@ -333,7 +333,7 @@ for _cluster in ${K8S_CLUSTERS}; do
 
 
     while (test $_ordinal -lt ${_numReplicas}) ; do
-        _pod="${K8S_STATEFUL_SET_NAME}-${_ordinal}.${_cluster}"
+        _pod="${K8S_POD_INSTANCE_NAME_PREFIX}${_ordinal}${K8S_POD_INSTANCE_NAME_SUFFIX}"
         
         # If we are printing a row representing the seed pod
         _seedIndicator=""
